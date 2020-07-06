e689630a77295079b81660f8764683add72a2bac
Mohamed Ndiaye <38785512+mohamedndiayedev@users.noreply.github.com>
2020-07-05 19:37:28 -1200

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,29 +1,14 @@
- 
-      <!-- FOOTER -->
-      <footer class="grid">
-        <!-- Footer - top -->
-        <!-- Image-->
-        <div class="s-12 l-3 m-row-3 margin-bottom background-image" style="background-image:url(img/img04.jpg)"></div>
-        
-        <div class="s-12 m-9 l-3 padding-2x margin-bottom background-dark">
-           <h2 class="text-strong text-uppercase">Who We Are?</h2>
-           <p>We are Pastef Malaysia, the official support of the President Ousmane Sonko sharing the same vision and spreading it in all around Asia. A better Senegal, our main mission.</p>
+ <!-- Footer -->
+ <footer class="sticky-footer bg-white">
+        <div class="container my-auto text-center">
+          <div class="copyright text-center my-auto">
+            <span>Pastef Asia &copy; 2020 - Version Beta 1.0</span>
+          </div>
         </div>
-        
-        <div class="s-12 m-9 l-3 padding-2x margin-bottom background-dark">
-           <h2 class="text-strong text-uppercase">Where We Are?</h2>
-           <img class="full-img" src="img/map.svg" alt=""/>
-        </div>
-        
-        <div class="s-12 m-9 l-3 padding-2x margin-bottom background-dark">
-           <h2 class="text-strong text-uppercase">We Are</h2>
-           <p><b class="text-primary margin-right-10"></b>PASTEF ASIA</p>
-           <p><b class="text-primary margin-right-10"></b> <a class="text-primary-hover" href="mailto:contact@sampledomain.com"></a></p>
-           <p><b class="text-primary margin-right-10"></b> <a class="text-primary-hover" href="mailto:office@sampledomain.com"></a></p>
-        </div>
-        
-        <!-- Footer - bottom -->
-        <div class="s-12 text-center margin-bottom">
-          <p class="text-size-12">Copyright 2020 - PASTEF ASIA</p>
-        </div>
-      </footer>                                  
+      </footer>
+      <!-- End of Footer -->
+
+      
+</body>
+
+</html>
